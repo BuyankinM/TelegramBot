7628368fe283a78c52c6401c21e8542c8db9754b
mbuyankin <mbuyankin@gmail.com>
2024-07-22 00:05:32 +0300

    Lesson 3. First working with RabbitMQ in Docker

--- dispatcher/src/main/java/ru/buyankin/services/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/ru/buyankin/services/impl/UpdateProducerImpl.java
@@ -1,6 +1,8 @@
 package ru.buyankin.services.impl;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.buyankin.services.UpdateProducer;
@@ -9,8 +11,16 @@ import ru.buyankin.services.UpdateProducer;
 @Log4j
 public class UpdateProducerImpl implements UpdateProducer {
 
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
