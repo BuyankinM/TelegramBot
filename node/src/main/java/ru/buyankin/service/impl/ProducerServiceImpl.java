302efb0ea2ab22d960b418f3c80cc6b79962e910
mbuyankin <mbuyankin@gmail.com>
2024-07-23 00:51:56 +0300

    Lesson 5. Add microservice Node

---
+++ node/src/main/java/ru/buyankin/service/impl/ProducerServiceImpl.java
@@ -0,0 +1,24 @@
+package ru.buyankin.service.impl;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.buyankin.service.ProduceService;
+
+import static ru.buyankin.model.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class ProducerServiceImpl implements ProduceService {
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public ProducerServiceImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void produceAnswer(SendMessage sendMessage) {
+        rabbitTemplate.convertAndSend(ANSWER_MESSAGE, sendMessage);
+    }
+}
