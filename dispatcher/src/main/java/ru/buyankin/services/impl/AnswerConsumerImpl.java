75eeb5d2841d1eea1f0ff4aa1e2c0c4498b4080a
mbuyankin <mbuyankin@gmail.com>
2024-09-11 23:24:34 +0300

    Lesson 14. Add web-hooks

--- dispatcher/src/main/java/ru/buyankin/services/impl/AnswerConsumerImpl.java
+++ dispatcher/src/main/java/ru/buyankin/services/impl/AnswerConsumerImpl.java
@@ -4,23 +4,23 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import ru.buyankin.controller.UpdateController;
+import ru.buyankin.controller.UpdateProcessor;
 import ru.buyankin.services.AnswerConsumer;
 
 import static ru.buyankin.model.RabbitQueue.ANSWER_MESSAGE;
 
 @Service
 public class AnswerConsumerImpl implements AnswerConsumer {
-    public final UpdateController updateController;
+    public final UpdateProcessor updateProcessor;
 
     @Autowired
-    public AnswerConsumerImpl(UpdateController updateController) {
-        this.updateController = updateController;
+    public AnswerConsumerImpl(UpdateProcessor updateProcessor) {
+        this.updateProcessor = updateProcessor;
     }
 
     @Override
     @RabbitListener(queues = ANSWER_MESSAGE)
     public void consume(SendMessage sendMessage) {
-        updateController.setView(sendMessage);
+        updateProcessor.setView(sendMessage);
     }
 }
