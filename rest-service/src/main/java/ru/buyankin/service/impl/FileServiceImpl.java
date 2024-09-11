c00b14a27461679a76e6e3901e12a78dec678f61
mbuyankin <mbuyankin@gmail.com>
2024-09-11 22:52:41 +0300

    Lesson 13.1 Delete saving tmp-files

--- rest-service/src/main/java/ru/buyankin/service/impl/FileServiceImpl.java
+++ rest-service/src/main/java/ru/buyankin/service/impl/FileServiceImpl.java
@@ -1,36 +1,29 @@
 package ru.buyankin.service.impl;
 
 import lombok.extern.log4j.Log4j;
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import ru.buyankin.dao.AppDocumentDAO;
 import ru.buyankin.dao.AppPhotoDAO;
 import ru.buyankin.entity.AppDocument;
 import ru.buyankin.entity.AppPhoto;
-import ru.buyankin.entity.BinaryContent;
 import ru.buyankin.service.FileService;
 import ru.buyankin.utils.CryptoTool;
 
-import java.io.File;
-import java.io.IOException;
-
 @Log4j
 @Service
-public class FileSrviveImpl implements FileService {
+public class FileServiceImpl implements FileService {
 
     private final AppDocumentDAO appDocumentDAO;
     private final AppPhotoDAO appPhotoDAO;
     private final CryptoTool cryptoTool;
 
     @Autowired
-    public FileSrviveImpl(AppDocumentDAO appDocumentDAO, AppPhotoDAO appPhotoDAO, CryptoTool cryptoTool) {
+    public FileServiceImpl(AppDocumentDAO appDocumentDAO, AppPhotoDAO appPhotoDAO, CryptoTool cryptoTool) {
         this.appDocumentDAO = appDocumentDAO;
         this.appPhotoDAO = appPhotoDAO;
         this.cryptoTool = cryptoTool;
     }
-
 
     @Override
     public AppDocument getDocument(String docId) {
@@ -48,19 +41,5 @@ public class FileSrviveImpl implements FileService {
             return null;
         }
         return appPhotoDAO.findById(id).orElse(null);
-    }
-
-    @Override
-    public FileSystemResource getFileSystemResource(BinaryContent binaryContent) {
-        try {
-            //TODO добавить генерацию имени временного файла
-            File temp = File.createTempFile("tempFile", ".bin");
-            temp.deleteOnExit();
-            FileUtils.writeByteArrayToFile(temp, binaryContent.getFileAsArrayOfBytes());
-            return new FileSystemResource(temp);
-        } catch (IOException e) {
-            log.error(e);
-            return null;
-        }
     }
 }
